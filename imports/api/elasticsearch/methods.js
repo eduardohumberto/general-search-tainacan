71cf7b75f801d8837406195cc6d6c2b2587231bc
DESKTOP-22M8AFR\desenvolvedor <eduardo.humberto1992@gmail.com>
2017-10-11 15:10:09 -0300

    begin advanced filters

--- imports/api/elasticsearch/methods.js
+++ imports/api/elasticsearch/methods.js
@@ -32,9 +32,12 @@ export const getFilters = function(aggregations,query){
             response = JSON.parse(response.content);
             if(response.hits.hits[0]){
                 var data = getMetadata( result.apiUrl+'/collections/'+collection.key+'/metadata');
-                //s
+
+                //if there is not metadata just return
                 if(data.length === 0)
                     return;
+
+                getItemsColletion(repo_items.key,collection.key,query);
 
                 resultCollection.name = response.hits.hits[0]._source.post_title;
                 resultCollection.itemsFound = collection.doc_count;
@@ -69,7 +72,7 @@ const getMetadata = function (api_url){
 }
 
 //buscando os itens da colecao
-const getItemsColletion = function(index,collection){
+const getItemsColletion = function(index,collection,query){
     var items = [];
     var jsonStr =  {
         "size": 10000,
@@ -95,8 +98,82 @@ const getItemsColletion = function(index,collection){
         data: jsonStr
     });
     response = JSON.parse(response.content);
-    _.each(response.hits.hits, function(item,index) {
-        items.push(item._source);
+    var counters = verifyMatch(query,response.hits.hits);
+    console.log('------');
+    console.log(counters);
+    //return items;
+}
+
+const verifyMatch = function(query,items){
+    var result ={
+        title:{
+            cont:0
+        },
+        post_content:{
+            cont:0
+        },
+        post_author:{
+            cont:0
+        },
+        terms:{
+            cont:0
+        }
+    }
+    _.each(items, function(item,index) {
+        //items.push(item._source);
+        _.each(item._source, function(value,index) {
+            switch (index){
+                case 'post_title':
+                    if(hasValue(query,value)){
+                        result.title.cont++;
+                    }
+                    break;
+                case 'post_content':
+                    if(hasValue(query,value)){
+                        result.post_content.cont++;
+                    }
+                    break;
+                case 'post_author':
+                    if(hasValue(query,value.display_name)){
+                        result.post_author.cont++;
+                    }
+                    break;
+                case 'terms':
+                    _.each(value.socialdb_category_type, function(category) {
+                        if(hasValue(query,category.name)){
+                            result.terms.cont++;
+                        }
+                    });
+                    break;
+                case 'post_meta':
+                    _.each(value, function(meta,meta_key) {
+                        _.each(meta, function(val) {
+                            if(val && hasValue(query,val)){
+                                if(result[meta_key]){
+                                   var obj = result[meta_key];
+                                   obj['cont']++;
+                                   result[meta_key] = obj;
+                                }else{
+                                    result[meta_key] = { cont: 1 }
+                                }
+                            }
+                        });
+                    });
+                    break;
+            }
+        })
     });
-    return items;
+    return result;
+}
+
+// verify if string in value
+const hasValue = function(string,value){
+    var array = string.split(' ');
+    var isMatch = false;
+    _.each(array, function(word){
+        if(value.indexOf(word)>=0){
+            isMatch = true;
+        }
+    });
+    return isMatch;
 }
